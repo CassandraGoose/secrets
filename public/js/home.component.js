514a0dd5d04e23779142b07ccaba77cc08919946
Cassandra Torske <cassandra.torske@gmail.com>
2017-04-06 09:46:31 -0600

    organized

--- public/js/home.component.js
+++ public/js/home.component.js
@@ -4,9 +4,20 @@ angular
 
   function HomeController($http, $stateParams, $state) {
     const vm = this
+    vm.login = login
+
       //base urL?
 
     vm.$onInit = function() {
       console.log('heyyyyyyybro');
     }
+
+    function login() {
+    $http.post(BaseURL + '/home', {
+        text: vm.secret.secret
+      })
+      .then(function(response) {
+        $state.go('itisdone')
+      })
+  }
   }
