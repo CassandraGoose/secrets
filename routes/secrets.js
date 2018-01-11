c9b3d2837e0e3372c925d76e03c4d1e4989a81ff
Cassandra Torske <cassandra.torske@gmail.com>
2018-01-11 14:24:50 -0700

    bug fix

--- routes/secrets.js
+++ routes/secrets.js
@@ -3,14 +3,12 @@ const router = express.Router()
 const knex = require('../db')
 
 router.get('/', (req, res) => {
-  console.log('fucking work');
   res.status(200).json({
     hello: 'thanks for finding me.'
   })
 })
 
 router.post('/', (req, res) => {
-  console.log(req.body.text)
   var theSecret = {
     text: req.body.text
   }
